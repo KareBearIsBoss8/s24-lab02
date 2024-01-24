23ae25043e9f10440950b3c40ca689216c951321
KareBearIsBoss8 <karolenazhou@karolenas-mbp-2.wifi.local.cmu.edu>
2024-01-23 20:02:12 -0500

    improve encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2);
+        Shape square = new Square(2);
+        Shape rectangle = new Rectangle(2,21);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer cir = new Renderer(circle);
+        Renderer squ = new Renderer(square);
+        Renderer rec = new Renderer(rectangle);
 
-        renderer.draw();
+        cir.draw();
+        squ.draw();
+        rec.draw();
     }
 }
